1d60ddc178c354eb5a7d8e08f1e74ba253f68be2
gunwing123 <73291366+gunwing123@users.noreply.github.com>
2021-05-16 15:46:00 +0900

    Update 코로나19 이후 일상 활동 변화.py

--- 코로나19 이후 일상 활동 변화.py
+++ 코로나19 이후 일상 활동 변화.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 import urllib.request as request 
 
-json_str = request.urlopen("url").read()#https://data.go.kr/data/15077856/fileData.do
+json_str = request.urlopen("url").read()#https://data.go.kr/data/15077856/fileData.do 에 들어가서 Api주소 받고 url에 넣기
 d = json.loads(json_str)
 
 down = [0,0,0,0,0,0,0,0,0,0]
@@ -12,30 +12,19 @@ case = [0,0,0,0,0,0,0,0,0,0]
 up = [0,0,0,0,0,0,0,0,0,0]
 name= [0,0,0,0,0,0,0,0,0,0]
 count = 0
-for e in  d['data']:
-    a = e['감소']
-    down[count] = a
-    count = count + 1
-count = 0
-for e in  d['data']:
-    a = e['변화 없음']
-    no[count] = a
-    count = count + 1
-count = 0
-for e in  d['data']:
-    a = e['사례수']
-    case[count] = a
-    count = count + 1
-count = 0
-for e in  d['data']:
-    a = e['증가']
-    up[count] = a
-    count = count + 1
-count = 0
-for e in  d['data']:
-    a = e['항목']
-    name[count] = a
-    count = count + 1
+while count<8:
+    for e in  d['data']:
+        a = e['감소']#감소 데이터
+        down[count] = a
+        a = e['변화 없음']
+        no[count] = a#변화 없음 데이터
+        a = e['증가']
+        up[count] = a#증가 데이터
+        a = e['사례수']
+        cas[count] = a#증가 데이터
+        a = e['항목']
+        name[count] = a#이름 데이터
+        count = count + 1
 count = len(name)
 
 a = 0
@@ -44,25 +33,24 @@ while a<count:
     no[a] = float(no[a])
     case[a] = float(case[a])
     up[a] = float(up[a])
-    a = a+1
+    a = a+1#자료형 변환 str→int
 
 plt.rc('font', family='NanumGothic')
 fig, ax = plt.subplots(2,2,constrained_layout=True)
-x = np.arange(len(name))
+plt.suptitle('코로나19 이후 일상 활동 변화', fontsize = 20)
+x = np.arange(len(name))#그래프 기본 바탕
 
 plt.subplot(221)
 plt.pie(case, labels = name, autopct = '%.1f%%')
-plt.title('사례수')
+plt.title('사례수')#사례수 그래프
 plt.subplot(222)
 plt.pie(up, labels = name, autopct = '%.1f%%')
-plt.title('증가 비율')
+plt.title('증가 비율')#증가 비율 그래프
 plt.subplot(223)
 plt.pie(no, labels = name, autopct = '%.1f%%')
-plt.title('유지 비율')
+plt.title('유지 비율')#유지 비율 그래프
 plt.subplot(224)
 plt.pie(down, labels = name, autopct = '%.1f%%')
-plt.title('감소 비율')
-
-plt.suptitle('코로나19 이후 일상 활동 변화', fontsize = 20)
+plt.title('감소 비율')#감소 비율 그래프
 
 plt.show()
