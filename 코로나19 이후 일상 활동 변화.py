baf161315e36a2a84a2bcb8665c2bcb192c59ca2
gunwing123 <73291366+gunwing123@users.noreply.github.com>
2021-05-20 19:34:55 +0900

    Update 코로나19 이후 일상 활동 변화.py

--- 코로나19 이후 일상 활동 변화.py
+++ 코로나19 이후 일상 활동 변화.py
@@ -20,8 +20,6 @@ while count<8:
         no[count] = a#변화 없음 데이터
         a = e['증가']
         up[count] = a#증가 데이터
-        a = e['사례수']
-        cas[count] = a#증가 데이터
         a = e['항목']
         name[count] = a#이름 데이터
         count = count + 1
@@ -31,26 +29,25 @@ a = 0
 while a<count:
     down[a] = float(down[a])
     no[a] = float(no[a])
-    case[a] = float(case[a])
     up[a] = float(up[a])
+    case[a] = down[a] - up[a]
     a = a+1#자료형 변환 str→int
 
 plt.rc('font', family='NanumGothic')
-fig, ax = plt.subplots(2,2,constrained_layout=True)
-plt.suptitle('코로나19 이후 일상 활동 변화', fontsize = 20)
+fig, ax = plt.subplots(2,2)
+plt.suptitle('코로나19 이후 일상 활동 변화', fontsize = 30)
 x = np.arange(len(name))#그래프 기본 바탕
 
 plt.subplot(221)
-plt.pie(case, labels = name, autopct = '%.1f%%')
-plt.title('사례수')#사례수 그래프
-plt.subplot(222)
-plt.pie(up, labels = name, autopct = '%.1f%%')
-plt.title('증가 비율')#증가 비율 그래프
+plt.pie(up, labels = name, autopct = '%.1f%%', textprops={'size':15})
+plt.title('증가 비율',fontsize = 20)#증가 비율 그래프
 plt.subplot(223)
-plt.pie(no, labels = name, autopct = '%.1f%%')
-plt.title('유지 비율')#유지 비율 그래프
+plt.pie(no, labels = name, autopct = '%.1f%%', textprops={'size':15})
+plt.title('유지 비율',fontsize = 20)#유지 비율 그래프
+plt.subplot(222)
+plt.pie(down, labels = name, autopct = '%.1f%%', textprops={'size':15})
+plt.title('감소 비율',fontsize = 20)#감소 비율 그래프
 plt.subplot(224)
-plt.pie(down, labels = name, autopct = '%.1f%%')
-plt.title('감소 비율')#감소 비율 그래프
-
+plt.pie(case, labels = name, autopct = '%.1f%%', textprops={'size':15})
+plt.title('감소 - 증가',fontsize = 20) #감소-증가 비율 그래프
 plt.show()
